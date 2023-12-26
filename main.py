b8562636f1ac53aa78eada62c1094e3af55d5565
yt050297 <yt050297@gmail.com>
2023-12-27 01:11:25 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from routers import route_todo
+from schemas import SuccessMsg
+
+
+
+app = FastAPI()
+app.include_router(route_todo.router)
+
+@app.get("/", response_model=SuccessMsg)
+def root():
+  return {"message": "welcome"}
---
+++ routers/route_todo.py
@@ -0,0 +1,53 @@
+from fastapi import APIRouter
+from fastapi import Request, Response, HTTPException
+from fastapi.encoders import jsonable_encoder
+from schemas import Todo, TodoBody, SuccessMsg
+from database import db_create_todo, db_get_todos, db_get_single_todo, db_update_todo, db_delete_todo
+from starlette.status import HTTP_201_CREATED
+from typing import List
+
+router = APIRouter()
+
+@router.post("/api/todo", response_model=Todo)
+async def create_todo(request: Request, response: Response, data: TodoBody):
+  todo = jsonable_encoder(data)
+  res = await db_create_todo(todo)
+  response.status_code = HTTP_201_CREATED
+  if res:
+    return res
+  else:
+    raise HTTPException(status_code=404, detail="Create task failed")
+  
+@router.get("/api/todo", response_model=List[Todo])
+async def get_todos():
+    res = await db_get_todos()
+    return res
+  
+@router.get("/api/todo/{id}", response_model=Todo)
+async def get_single_todo(id: str):
+  res = await db_get_single_todo(id)
+  if res:
+    return res
+  raise HTTPException(
+    status_code=404, 
+    detail="Task of ID:{id} doesn't exist"
+  )
+  
+@router.put("/api/todo/{id}", response_model=Todo)
+async def update_todo(id :str, data: TodoBody):
+  todo = jsonable_encoder(data)
+  res = await db_update_todo(id, todo)
+  if res:
+    return res
+  raise HTTPException(
+    status_code=404, detail="update task failed"
+  )
+  
+@router.delete("/api/todo/{id}", response_model=SuccessMsg)
+async def delete_todo(id: str):
+  res = await db_delete_todo(id)
+  if res:
+    return {"message": "Successfully deleted"}
+  raise HTTPException(
+    status_code=404, detail="Delete task failed"
+  )
